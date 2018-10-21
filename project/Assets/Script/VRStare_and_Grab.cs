70a077cfc4196deb552eb3db4e1caf2c8de40ca4
joaobernardo17 <berninhas17@gmail.com>
2018-10-21 04:08:28 +0100

    Reticle still not 100% & added Deadpool

---
+++ project/Assets/Script/VRStare_and_Grab.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRStare_and_Grab : MonoBehaviour {
+
+    public float stare_time = 0f; // timer 
+    public Transform VRHand;
+    public Rigidbody TargetObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        stare_time = stare_time + Time.deltaTime;
+
+        if (stare_time >= 3f) // once a certain amount of time has passed, the object will be 'grabbed'
+        {
+            GrabObject();
+        }
+	}
+
+    public void ResetStareTime()
+    {
+        stare_time = 0f;
+    }
+
+    public void GrabObject()
+    {
+        TargetObject.transform.parent = VRHand.transform;
+    }
+}
