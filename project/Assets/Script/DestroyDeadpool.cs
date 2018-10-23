5338c66f7c510ebbf9b51abaa292ab4b1af6c1aa
Telma Marisa Neto Barragão <telmabarragao@MacBook-Pro-de-Telma.local>
2018-10-23 17:49:55 +0100

    shit

--- project/Assets/Script/DestroyDeadpool.cs
+++ project/Assets/Script/DestroyDeadpool.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class DestroyDeadpool : MonoBehaviour {
 
+  public GameObject flame;
+
     void OnCollisionEnter(Collision col){
         if (col.gameObject.tag.Equals("DeadPool"))
         {
+          Vector3 pos = col.gameObject.transform.position;
             Destroy(col.gameObject);
+
+            Quaternion q = new Quaternion(-0.5f,0f,0f,1f);
+            GameObject obj1 = Instantiate(flame, pos, q);
+            pos += new Vector3(0f,5f,0f);
+            GameObject obj2 = Instantiate(flame, pos, q);
+            pos += new Vector3(0f,5f,0f);
+            GameObject obj3 = Instantiate(flame, pos, q);
+
+            Destroy(obj1, 5);
+            Destroy(obj2, 5);
+            Destroy(obj3, 5);
+
         }
     }
 
