47f3d3f12345ba801d84bb6015f0218b0e4e967a
joaobernardo17 <berninhas17@gmail.com>
2018-10-23 14:48:21 +0100

    Walk bug fixes, destroyDeadpool added but not working

---
+++ project/Assets/Script/DestroyDeadpool.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDeadpool : MonoBehaviour {
+
+    void OnEnterCollision(Collider col){
+        if (col.gameObject.name == "deadpool_reference.001")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+}
--- project/Assets/Script/VRLookWalk.cs
+++ project/Assets/Script/VRLookWalk.cs
@@ -12,6 +12,7 @@ public class VRLookWalk : MonoBehaviour
     public float speed = 3.0f;
 
     public bool moveforward;
+    public bool movebackward;
 
     private CharacterController cc;
 
@@ -24,23 +25,34 @@ public class VRLookWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("x "+vrCamera.eulerAngles.x);
-        Debug.Log("y "+vrCamera.eulerAngles.y);
-        Debug.Log("z "+vrCamera.eulerAngles.z);
 
         if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
         {
             moveforward = false;
+            movebackward = true;
+        }
+        else if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x > 0.0f)
+        {
+            moveforward = true;
+            movebackward = false;
         }
         else
         {
-            moveforward = true;
+            moveforward = false;
+            movebackward = false;
         }
         if (moveforward == true)
         {
-            Vector3 forward = vrCamera.TransformDirection(new Vector3(Vector3.forward.x, 0f, Vector3.forward.z));
+            Vector3 forward = vrCamera.TransformDirection(new Vector3(Vector3.forward.x, Vector3.forward.y, Vector3.forward.z));
 
             cc.SimpleMove(forward * speed);
         }
+        else if (movebackward == true)
+        {
+            Vector3 backward = vrCamera.TransformDirection(new Vector3(-Vector3.forward.x, Vector3.forward.z, -Vector3.forward.z));
+
+            cc.SimpleMove(backward * speed);
+        }
+        else { }
     }
 }
