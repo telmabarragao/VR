33b983af695106c06dd31644224d9a911bc6b670
Telma Marisa Neto Barragão <telmabarragao@MacBook-Pro-de-Telma.local>
2018-11-19 16:31:46 +0000

    new

---
+++ Quiz/Assets/button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class button : MonoBehaviour, IVirtualButtonEventHandler {
+
+
+    private GameObject vButton;
+	// Use this for initialization
+	void Start () {
+
+        vButton = GameObject.Find("EntButton");
+        vButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        Debug.Log("OLA");
+
+	}
+
+    public void OnButtonPressed (VirtualButtonBehaviour vb){
+
+        Debug.Log("pressed");
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+
+    }
+
+}
