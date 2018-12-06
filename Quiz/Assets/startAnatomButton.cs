f8ccd7fd625e7b6a943df8db28a82a6d41ee583c
martadmendes <marta.durand@gmail.com>
2018-12-06 19:20:51 +0000

    Add anatomy scene with new virtual button implementation

---
+++ Quiz/Assets/startAnatomButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class startAnatomButton : MonoBehaviour, IVirtualButtonEventHandler {
+
+
+    public GameObject anatomyBtnObject;
+    //public Animator btnAnim;
+
+	void Start () {
+        anatomyBtnObject = GameObject.Find("StartQuizButton");
+        anatomyBtnObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        //btnAnim.GetComponent<Animator>();
+		
+	}
+	
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+        //setCategory(nameClickedButton);
+        Debug.Log("button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        Debug.Log("button released");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+}
