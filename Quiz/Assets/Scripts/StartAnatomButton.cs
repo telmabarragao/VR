b9261af3be8c6cafb6cee0548f81cfedcf21d3e1
martadmendes <marta.durand@gmail.com>
2018-12-08 01:03:02 +0000

    Improved image target detection, virtual button interaction and game start ui

---
+++ Quiz/Assets/Scripts/StartAnatomButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class StartAnatomButton : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject anatomyBtnObject;
+    public GameObject Anatomy;
+    public GameObject EntertainmentImageTarget;
+    public GameObject AnatomyImageTarget;
+    public GameObject CategorySelectionText;
+
+
+    void Start () {
+        anatomyBtnObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+	}
+	
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {  //esta funcao tem que estar aqui anyway, mesmo que nao faca nada.
+        Debug.Log("button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {  //same com esta
+
+        CategorySelectionText.SetActive(false);         //retira o texto para escolher categoria
+        Anatomy.SetActive(true);                        //activa o game object para o board da categoria
+
+        AnatomyImageTarget.GetComponent<ImageTargetBehaviour>().enabled = false;           //faz disable a deteccao de image targets
+        AnatomyImageTarget.SetActive(false);                                               //e aos game objects relativos aos image targets
+        EntertainmentImageTarget.GetComponent<ImageTargetBehaviour>().enabled = false;     //para nao reconhecer nada durante o jogo
+        EntertainmentImageTarget.SetActive(false);                                         //depois reactivaremos mais tarde no fim para recomecar
+
+        Debug.Log("button released");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+}
---
+++ Quiz/Assets/Scripts/StartEntertButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class StartEntertButton : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject entertBtnObject;
+    public GameObject Entertainment;
+    public GameObject AnatomyImageTarget;
+    public GameObject EntertainmentImageTarget;
+    public GameObject CategorySelectionText;
+
+
+    void Start() {
+        entertBtnObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {  //esta funcao tem que estar aqui anyway, mesmo que nao faca nada.
+        Debug.Log("button pressed"); 
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) { //same com esta
+        
+        CategorySelectionText.SetActive(false);      //retira o texto para escolher categoria
+        Entertainment.SetActive(true);               //activa o game object para o board da categoria
+
+        AnatomyImageTarget.GetComponent<ImageTargetBehaviour>().enabled = false;           //faz disable a deteccao de image targets
+        AnatomyImageTarget.SetActive(false);                                               //e aos game objects relativos aos image targets
+        EntertainmentImageTarget.GetComponent<ImageTargetBehaviour>().enabled = false;     //para nao reconhecer nada durante o jogo
+        EntertainmentImageTarget.SetActive(false);                                         //depois reactivaremos mais tarde no fim para recomecar
+ 
+        Debug.Log("button released");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+}
